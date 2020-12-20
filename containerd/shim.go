f31cd02da1bc0e7e5fad7da51545a48daa0bf3b0
Samuel Karp <me@samuelkarp.com>
2020-12-19 18:04:01 -0800

    shim: skeleton logging

--- containerd/shim.go
+++ containerd/shim.go
@@ -3,9 +3,12 @@ package containerd
 import (
 	"context"
 
+	"github.com/containerd/containerd/errdefs"
+	"github.com/containerd/containerd/log"
 	"github.com/containerd/containerd/runtime/v2/shim"
 	"github.com/containerd/containerd/runtime/v2/task"
 	"github.com/gogo/protobuf/types"
+	"github.com/sirupsen/logrus"
 )
 
 func NewService(ctx context.Context, id string, publisher shim.Publisher, shutdown func()) (shim.Shim, error) {
@@ -29,77 +32,101 @@ type service struct {
 }
 
 func (s service) State(ctx context.Context, req *task.StateRequest) (*task.StateResponse, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("STATE")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) Create(ctx context.Context, req *task.CreateTaskRequest) (*task.CreateTaskResponse, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("CREATE")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) Start(ctx context.Context, req *task.StartRequest) (*task.StartResponse, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("START")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) Delete(ctx context.Context, req *task.DeleteRequest) (*task.DeleteResponse, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("DELETE")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) Pids(ctx context.Context, req *task.PidsRequest) (*task.PidsResponse, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("PIDS")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) Pause(ctx context.Context, req *task.PauseRequest) (*types.Empty, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("PAUSE")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) Resume(ctx context.Context, req *task.ResumeRequest) (*types.Empty, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("RESUME")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) Checkpoint(ctx context.Context, req *task.CheckpointTaskRequest) (*types.Empty, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("CHECKPOINT")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) Kill(ctx context.Context, req *task.KillRequest) (*types.Empty, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("KILL")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) Exec(ctx context.Context, req *task.ExecProcessRequest) (*types.Empty, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("EXEC")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) ResizePty(ctx context.Context, req *task.ResizePtyRequest) (*types.Empty, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("RESIZEPTY")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) CloseIO(ctx context.Context, req *task.CloseIORequest) (*types.Empty, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("CLOSEIO")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) Update(ctx context.Context, req *task.UpdateTaskRequest) (*types.Empty, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("UPDATE")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) Wait(ctx context.Context, req *task.WaitRequest) (*task.WaitResponse, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("WAIT")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) Stats(ctx context.Context, req *task.StatsRequest) (*task.StatsResponse, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("STATS")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) Connect(ctx context.Context, req *task.ConnectRequest) (*task.ConnectResponse, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("CONNECT")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) Shutdown(ctx context.Context, req *task.ShutdownRequest) (*types.Empty, error) {
-	panic("implement me")
+	log.G(ctx).WithField("req", req).Warn("SHUTDOWN")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) Cleanup(ctx context.Context) (*task.DeleteResponse, error) {
-	panic("implement me")
+	log.G(ctx).Warn("CLEANUP")
+	return nil, errdefs.ErrNotImplemented
 }
 
 func (s service) StartShim(ctx context.Context, id, containerdBinary, containerdAddress, containerdTTRPCAddress string) (string, error) {
-	panic("implement me")
+	log.G(ctx).WithFields(logrus.Fields{
+		"id":                     id,
+		"containerdBinary":       containerdBinary,
+		"containerdAddress":      containerdAddress,
+		"containerdTTRPCAddress": containerdTTRPCAddress,
+	}).Warn("STARTSHIM")
+	return "", errdefs.ErrNotImplemented
 }
--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/opencontainers/go-digest v1.0.0
 	github.com/opencontainers/image-spec v1.0.1
 	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/cobra v1.0.0
 	github.com/stretchr/testify v1.5.1
 	golang.org/x/sys v0.0.0-20201201145000-ef89a241ccb3
